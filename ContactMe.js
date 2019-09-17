b6eb1997fc067d221d7e18e342c17eea2c12faf4
jordondoug2019 <jdouglas2015@gmail.com>
2019-09-17 18:25:39 -0500

    edit 11

---
+++ ContactMe.js
@@ -0,0 +1,77 @@
+let about_me=document.getElementById("aboutMe");
+let project_menu=document.getElementById("projects");
+let skills_menu=document.getElementById("skills");
+let education_menu=document.getElementById("education");
+let resume_menu=document.getElementById("resume");
+let youtube_menu=document.getElementById("youtube");
+let contact_menu=document.getElementById("contactMe");
+
+about_me.addEventListener('mouseenter',function ()
+{
+    about_me.height=100;
+    about_me.width=100;
+});
+about_me.addEventListener('mouseleave',function()
+{
+    about_me.height=50 ;
+    about_me.width=50;
+});
+project_menu.addEventListener('mouseenter',function()
+{
+    project_menu.height=100;
+    project_menu.width=100;
+});
+project_menu.addEventListener('mouseleave',function ()
+{
+    project_menu.height=50;
+    project_menu.width=50;
+});
+skills_menu.addEventListener('mouseenter',function(){
+    skills_menu.height=100;
+    skills_menu.width=100;
+});
+skills_menu.addEventListener('mouseleave',function ()
+{
+    skills_menu.height=50;
+    skills_menu.width=50;
+});
+education_menu.addEventListener('mouseenter',function ()
+{
+    education_menu.height=100;
+    education_menu.width=100;
+});
+education_menu.addEventListener('mouseleave',function ()
+{
+    education_menu.height=50;
+    education_menu.width=50;
+});
+resume_menu.addEventListener('mouseenter',function ()
+{
+    resume_menu.height=100;
+    resume_menu.width=100;
+});
+resume_menu.addEventListener('mouseleave',function ()
+{
+    resume_menu.height=50;
+    resume_menu.width=50;
+});
+youtube_menu.addEventListener('mouseenter',function ()
+{
+    youtube_menu.height=100;
+    youtube_menu.width=100;
+});
+youtube_menu.addEventListener('mouseleave',function()
+{
+    youtube_menu.height=50;
+    youtube_menu.width=50;
+});
+contact_menu.addEventListener('mouseenter',function ()
+{
+    contact_menu.height=100;
+    contact_menu.width=100;
+});
+contact_menu.addEventListener('mouseleave',function ()
+{
+    contact_menu.height=50;
+    contact_menu.width=50;
+});
