b211132dcab4b64780c8c7abd01b26518713858f
jordondoug2019 <jdouglas2015@gmail.com>
2020-01-22 11:12:20 -0600

    portfolio edits

--- home.js
+++ home.js
@@ -8,7 +8,8 @@ let education_menu=document.getElementById("education");
 let resume_menu=document.getElementById("resume");
 let youtube_menu=document.getElementById("youtube");
 let contact_menu=document.getElementById("contactMe");
-x.addEventListener('mouseover',function ()
+
+x.addEventListener('mouseenter',function ()
 {
    // x=pic_flip[1];
    // i++;
@@ -25,8 +26,8 @@ x.addEventListener('mouseover',function ()
 });
 about_me.addEventListener('mouseenter',function ()
 {
-    about_me.height=110;
-    about_me.width=110;
+   about_me.height= 110;
+   about_me.width=110;
 });
 about_me.addEventListener('mouseleave',function()
 {
@@ -51,36 +52,6 @@ skills_menu.addEventListener('mouseleave',function ()
 {
     skills_menu.height=75;
     skills_menu.width=75;
-});
-education_menu.addEventListener('mouseenter',function ()
-{
-    education_menu.height=110;
-    education_menu.width=110;
-});
-education_menu.addEventListener('mouseleave',function ()
-{
-    education_menu.height=75;
-    education_menu.width=75;
-});
-resume_menu.addEventListener('mouseenter',function ()
-{
-    resume_menu.height=110;
-    resume_menu.width=110;
-});
-resume_menu.addEventListener('mouseleave',function ()
-{
-    resume_menu.height=75;
-    resume_menu.width=75;
-});
-youtube_menu.addEventListener('mouseenter',function ()
-{
-    youtube_menu.height=110;
-    youtube_menu.width=110;
-});
-youtube_menu.addEventListener('mouseleave',function()
-{
-    youtube_menu.height=90;
-    youtube_menu.width=105;
 });
 contact_menu.addEventListener('mouseenter',function ()
 {
